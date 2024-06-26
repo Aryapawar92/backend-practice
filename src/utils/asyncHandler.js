1a973d9c403d8544c5c0462b531f60faf0d22027
arya <aryapawar92@gmail.com>
2024-06-26 14:56:59 +0530

    after jwt , bcrypt , aggregate

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,7 @@
-const asynchandler = () => {
-
+const asynchandler = (requesthandler) => {
+    return (res,req,next) => {
+        Promise.resolve(requesthandler(req,res,next)).catch((err) => next(err))
+    }
 }
 
 export {asynchandler}
